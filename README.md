69a3bb614c8a51b622153ebf907eca0fd32e3bd9
ganeshpatil0101 <ganeshpatil0101@gmail.com>
2022-04-14 22:55:50 +0530

    Mutation Observer Test

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# js-vwmtzn
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vwmtzn)
+# Javascript features
--- index.js
+++ index.js
@@ -2,5 +2,42 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const target = document.getElementById('app');
+target.innerHTML = `<h1>Mutation Observer</h1>`;
+
+const log = document.querySelector('#log');
+
+function changeDom() {
+  const s = document.createElement('span');
+  s.innerHTML = ' Child El ';
+  target.appendChild(s);
+}
+
+function changeAttribute() {
+  target.style.color =
+    target.style.color == 'yellow'
+      ? 'red'
+      : target.style.color == 'red'
+      ? 'blue'
+      : 'yellow';
+}
+
+document.querySelector('#changeDom').addEventListener('click', changeDom);
+
+document
+  .querySelector('#changeAttribute')
+  .addEventListener('click', changeAttribute);
+
+const config = { attributes: true, childList: true };
+const callBack = (mutationList) => {
+  for (const m of mutationList) {
+    if (m.type === 'childList') {
+      log.innerHTML += ' Child List Updated \n';
+    } else if (m.type === 'attributes') {
+      log.innerHTML += ' Attribute Updated \n';
+    }
+  }
+};
+
+const observer = new MutationObserver(callBack);
+observer.observe(target, config);
